6624db3831b9a4b75d795de6a261e4d6a24507ed
coffeiz <coffeiz216@gmail.com>
2026-08-01 16:57:02 +0800

    接入已完成卡片detach生命周期

--- frontend/src/views/Projects/components/done/DoneCardList.vue
+++ frontend/src/views/Projects/components/done/DoneCardList.vue
@@ -3,18 +3,37 @@
     class="month-cards"
     :class="{ 'recent-card-list': recent }"
   >
-    <div v-for="project in projects" :key="`project-${project.id}`" class="done-card-item" :data-layout-key="`project-${project.id}`" data-layout-role="card">
-      <ProjectCard :project="project" @click="$emit('card-click', project)" />
-    </div>
+    <Teleport
+      v-for="project in projects"
+      :key="`project-${project.id}`"
+      to="body"
+      :disabled="!isDetached(String(project.id))"
+    >
+      <div class="done-card-item" :data-layout-key="`project-${project.id}`" data-layout-role="card">
+        <ProjectCard :project="project" @click="$emit('card-click', project)" />
+      </div>
+    </Teleport>
   </div>
 </template>
 
 <script setup lang="ts">
-import type { PropType } from 'vue'
+import { onUnmounted, ref, type PropType } from 'vue'
 import type { Project } from '@/types/project'
+import { runtime } from '@/interaction/runtime'
 import ProjectCard from '../ProjectCard.vue'
 
 defineEmits(['card-click'])
+
+const ownershipVersion = ref(0)
+const stopOwnershipSubscription = runtime.owner.subscribe(() => {
+  ownershipVersion.value += 1
+})
+onUnmounted(stopOwnershipSubscription)
+
+function isDetached(projectId: string): boolean {
+  ownershipVersion.value
+  return runtime.owner.isControlled(projectId)
+}
 
 defineProps({
   projects: { type: Array as PropType<Project[]>, default: () => [] },
