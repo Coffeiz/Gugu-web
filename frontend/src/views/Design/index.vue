af69a4762fa53c4dffd13bf38b2375a4dee06457
Coffeiz <coffeiz216@gmail.com>
2026-08-16 10:45:59 +0800

    feat(design): show new theme component contracts

--- frontend/src/views/Design/index.vue
+++ frontend/src/views/Design/index.vue
@@ -1,7 +1,9 @@
 <template>
   <DesignSystemPage />
+  <ThemeContractsPreview />
 </template>
 
 <script setup lang="ts">
 import DesignSystemPage from './components/DesignSystemPage.vue'
+import ThemeContractsPreview from './components/ThemeContractsPreview.vue'
 </script>
